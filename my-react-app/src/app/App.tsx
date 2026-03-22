7d625bc75a85bd19f05b26c119a17b48eac14a5f
SONOFINFINITE <ksmirnov23@gmail.com>
2026-03-22 17:14:19 +0300

    feat: Initialize the main application component with React Router and define core page routes.

--- my-react-app/src/app/App.tsx
+++ my-react-app/src/app/App.tsx
@@ -1,4 +1,5 @@
-import { Routes, Route } from 'react-router-dom';
+import { Routes, Route, useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
 import './styles/global.scss';
 import { HomePage } from '../pages/home/HomePage';
 import { CatalogPage } from '../pages/catalog/CatalogPage';
@@ -19,27 +20,40 @@ import { ForgotPassPage } from '../pages/forgotpass/ForgotPassPage';
 import { SignupPage } from '../pages/signup/SignupPage';
 import { AccountPage } from '../pages/account/AccountPage';
 
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
+
 export function App() {
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/catalog/*" element={<CatalogPage />} />
-      <Route path="/collection" element={<CollectionsPage />} />
-      <Route path="/sale" element={<PromotionsPage />} />
-      <Route path="/cart" element={<CartPage />} />
-      <Route path="/favorites" element={<FavoritesPage />} />
-      <Route path="/product/:id" element={<ProductPage />} />
-      <Route path="/checkout" element={<CheckoutPage />} />
-      <Route path="/blog" element={<BlogPage />} />
-      <Route path="/blog/:slug" element={<ArticlePage />} />
-      <Route path="/payment" element={<PaymentPage />} />
-      <Route path="/delivery" element={<DeliveryPage />} />
-      <Route path="/privacy" element={<PrivacyPage />} />
-      <Route path="/company" element={<CompanyPage />} />
-      <Route path="/contacts" element={<ContactsPage />} />
-      <Route path="/forgotpass" element={<ForgotPassPage />} />
-      <Route path="/signup" element={<SignupPage />} />
-      <Route path="/personal" element={<AccountPage />} />
-    </Routes>
+    <>
+      <ScrollToTop />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/catalog/*" element={<CatalogPage />} />
+        <Route path="/collection" element={<CollectionsPage />} />
+        <Route path="/sale" element={<PromotionsPage />} />
+        <Route path="/cart" element={<CartPage />} />
+        <Route path="/favorites" element={<FavoritesPage />} />
+        <Route path="/product/:id" element={<ProductPage />} />
+        <Route path="/checkout" element={<CheckoutPage />} />
+        <Route path="/blog" element={<BlogPage />} />
+        <Route path="/blog/:slug" element={<ArticlePage />} />
+        <Route path="/payment" element={<PaymentPage />} />
+        <Route path="/delivery" element={<DeliveryPage />} />
+        <Route path="/privacy" element={<PrivacyPage />} />
+        <Route path="/company" element={<CompanyPage />} />
+        <Route path="/contacts" element={<ContactsPage />} />
+        <Route path="/forgotpass" element={<ForgotPassPage />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/personal" element={<AccountPage />} />
+      </Routes>
+    </>
   );
 }
